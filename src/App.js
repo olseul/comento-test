c007f97c8f00b94e1905e6a3bb3df79d816e7714
olseul <seul9085@naver.com>
2022-09-21 18:13:57 +0900

    feat: 네비게이션 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Navigation from './components/Navigation'
 
 function App() {
-  return <div className="App">프로젝트 시작</div>;
+  return (
+    <div className="App">
+      <Navigation/>
+    </div>
+  );
 }
 
 export default App;
